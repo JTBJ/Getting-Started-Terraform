f51986df4ece30ecdd0566f7bc75246ba5f75d4d
root <root@ip-172-31-23-40.ec2.internal>
2025-09-12 20:23:22 +0000

    added tags to lb resources

--- base_web_app_v2/loadBalancer.tf
+++ base_web_app_v2/loadBalancer.tf
@@ -17,6 +17,8 @@ resource "aws_lb_target_group" "nginx" {
   port     = var.aws_security_group_ingress_to_from_port
   protocol = "HTTP"
   vpc_id   = aws_vpc.app.id
+
+  tags = local.common_tags
 }
 
 # aws_lb_listener
@@ -29,6 +31,8 @@ resource "aws_lb_listener" "nginx" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.nginx.arn
   }
+
+  tags = local.common_tags
 }
 
 # aws_lb_target_group_attachment
